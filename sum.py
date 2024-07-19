0d9624e07ca1afd2e98aec1ea45bb587569e8dc0
Manish Raj <120162412+raj13manish@users.noreply.github.com>
2024-07-19 10:28:04 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,36 @@
+
+
+
+
+# number1 = int (input("enter number 1 ="))
+# number2 = int (input("enter number 2 ="))
+# sum = number1+number2
+# print("the sum of this two number is ",sum)
+
+number1 = 5
+number2= 5
+sum = number1+number2
+sub = number1-number2
+div = number1/number2
+mul = number1*number2
+mod = number1%number2
+
+print("sum of two number is ",sum)
+print("sub of two number is ",sub)
+print("div of two number is ",div)
+print("mul of two number is ",mul)
+print("mod of two number is ",mod)
+
+
+
+
+
+# opretor 
+# aritmetic 
+# assiment 
+# comparison
+# logical
+# identity
+# membership
+# bitwise 
+
